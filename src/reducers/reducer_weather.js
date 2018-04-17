95cec0c26968c6165bef2b0845eebc9b86cc08b2
Meagan Blais <meaganblais@gmail.com>
2018-04-17 16:07:50 -0400

    finishing up weather reducer to ensure state mutation in reducer is avoided

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,15 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
-  
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  // console.log('Action received', action);
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // // es5
+      // return state.concat([action.payload.data]);
+      // es6 is nearly identical results but puts new entry in front
+      // ... means it might be an array so take all the entries
+      //  inside of it and insert it into this new array
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
