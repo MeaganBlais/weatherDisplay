0f2808ea06a875f4d754b8b59e7b2fa0e3337cc1
Meagan Blais <meaganblais@gmail.com>
2018-04-17 12:23:42 -0400

    setting up weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='3de179f9827af9aff6418b26c4f26fea';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // seperated into variable to keep our action types consistent 
 // between action creaters & reducers ... simply call variable in each
@@ -10,6 +10,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  console.log('Request:', request);
 
   // payload is an optional property that goes along with actions that
   // contains some additional data that describes that particular action
