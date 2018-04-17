6831e38746889ff73d3e5d06a29cc0c337bae627
Meagan Blais <meaganblais@gmail.com>
2018-04-17 18:37:58 -0400

    making the charts a resuable component & adding labels for the unit types

---
+++ src/components/chart.js
@@ -0,0 +1,22 @@
+/* this can be a function based component because we don't need to make any use of state, 
+component state, some props come in and we render a component */
+
+import _ from 'lodash';
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  return _.round(_.sum(data) / data.length-1);
+}
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
+}
