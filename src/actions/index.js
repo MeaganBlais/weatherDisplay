ab46a7bd36a067ecfd54ea1a21b90d158bf6624b
Meagan Blais <meaganblais@gmail.com>
2018-04-17 12:06:09 -0400

    setting up axios package and redux promise to fetch weather information

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY ='3de179f9827af9aff6418b26c4f26fea';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// seperated into variable to keep our action types consistent 
+// between action creaters & reducers ... simply call variable in each
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  // payload is an optional property that goes along with actions that
+  // contains some additional data that describes that particular action
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
