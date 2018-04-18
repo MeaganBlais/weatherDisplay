98b00b9f02b60f81771162576ab1d18539f6dea8
Meagan Blais <meaganblais@gmail.com>
2018-04-18 14:13:23 -0400

    changing country code to canada

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,10 +8,10 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},ca`;
   const request = axios.get(url);
 
-  console.log('Request:', request);
+  // console.log('Request:', request);
 
   // payload is an optional property that goes along with actions that
   // contains some additional data that describes that particular action
